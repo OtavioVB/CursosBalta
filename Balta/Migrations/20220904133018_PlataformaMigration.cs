c0dc8d9d474c6dbe66a78766f977d97b9a5cb4fe
OtavioVB <otaviovb.developer@gmail.com>
2022-09-04 10:31:29 -0300

    refactor(migrations)

---
+++ Balta/Migrations/20220904133018_PlataformaMigration.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Balta.Migrations
+{
+    public partial class PlataformaMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cursos",
+                columns: table => new
+                {
+                    Identificador = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titulo = table.Column<string>(type: "TEXT", nullable: false),
+                    Tag = table.Column<int>(type: "INTEGER", nullable: false),
+                    Duracao = table.Column<TimeSpan>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cursos", x => x.Identificador);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Modulos",
+                columns: table => new
+                {
+                    Identificador = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    IdentificadorCurso = table.Column<int>(type: "INTEGER", nullable: false),
+                    Titulo = table.Column<string>(type: "TEXT", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: false),
+                    Ordem = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modulos", x => x.Identificador);
+                    table.ForeignKey(
+                        name: "FK_Modulos_Cursos_IdentificadorCurso",
+                        column: x => x.IdentificadorCurso,
+                        principalTable: "Cursos",
+                        principalColumn: "Identificador",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Aulas",
+                columns: table => new
+                {
+                    Identificador = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    IdentificadorModulo = table.Column<int>(type: "INTEGER", nullable: false),
+                    Titulo = table.Column<string>(type: "TEXT", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: false),
+                    Url = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aulas", x => x.Identificador);
+                    table.ForeignKey(
+                        name: "FK_Aulas_Modulos_IdentificadorModulo",
+                        column: x => x.IdentificadorModulo,
+                        principalTable: "Modulos",
+                        principalColumn: "Identificador",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Aulas_IdentificadorModulo",
+                table: "Aulas",
+                column: "IdentificadorModulo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Modulos_IdentificadorCurso",
+                table: "Modulos",
+                column: "IdentificadorCurso");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aulas");
+
+            migrationBuilder.DropTable(
+                name: "Modulos");
+
+            migrationBuilder.DropTable(
+                name: "Cursos");
+        }
+    }
+}
