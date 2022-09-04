254dd90953d6360e57f027a3258f1b701d7c7837
OtavioVB <otaviovb.developer@gmail.com>
2022-09-04 20:17:52 -0300

    refactor(database): `directory`

--- Balta/Program.cs
+++ Balta/Program.cs
@@ -19,13 +19,14 @@ public class Program
         builder.Services.AddTransient<IRepositoryModulo, ModuloRepository>();
         builder.Services.AddTransient<IRepositoryAula, AulaRepository>();
 
-        builder.Services.AddDbContext<DataContext>();
-        builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString));
+        builder.Services.AddDbContext<DataContext>(); //"{_appEnv.ApplicationBasePath}/" 
+        builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(AppContext.BaseDirectory + "/" +connectionString));
         builder.Services.AddDatabaseDeveloperPageExceptionFilter();
         builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
           
         builder.Services.AddRazorPages();
         var app = builder.Build();
+        app.UseMigrationsEndPoint();
         app.UseHttpsRedirection();
         app.UseStaticFiles();
         app.UseRouting();
