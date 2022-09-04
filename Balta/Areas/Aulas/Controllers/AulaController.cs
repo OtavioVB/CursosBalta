84e2ad683c86baca71692b53e9fc68b33e87c496
OtavioVB <otaviovb.developer@gmail.com>
2022-09-04 11:36:36 -0300

    refactor(views): `view imports`

--- Balta/Areas/Aulas/Controllers/AulaController.cs
+++ Balta/Areas/Aulas/Controllers/AulaController.cs
@@ -15,7 +15,7 @@ public class AulaController : Controller
     public IActionResult Criar([FromServices] IRepositoryModulo repositoryModulo)
     {
         ViewBag.Modulos = repositoryModulo.ListarModulos();
-        return View();
+        return View("Pages/Aula/Criar.cshtml");
     }
 
     [Route("Adicionar")]
@@ -27,7 +27,7 @@ public class AulaController : Controller
         aula.IdentificadorModulo = criarNovaAula.IdentificadorModulo;
         aula.Url = UrlFunctions.ObterUrlAmigavel(criarNovaAula.Titulo);
         repositoryAula.CriarNovaAula(aula);
-        return View();
+        return View("Pages/Aula/Criar.cshtml");
     }
 
     [Route("Visualizar/{url}")]
@@ -39,7 +39,7 @@ public class AulaController : Controller
             if (aula != null)
             {
                 ViewBag.Aula = aula;
-                return View();
+                return View("Pages/Aula/Visualizar.cshtml");
             }
             else
             {
--- Balta/Areas/Modulos/Controllers/ModuloController.cs
+++ Balta/Areas/Modulos/Controllers/ModuloController.cs
@@ -15,7 +15,7 @@ public class ModuloController : Controller
     public IActionResult Index([FromServices] IRepositoryModulo moduloRepository)
     {
         ViewBag.listaModulos = moduloRepository.ListarModulos();
-        return View();
+        return View("Pages/Modulo/Index.cshtml");
     }
 
     [Route("Adicionar")]
@@ -36,6 +36,6 @@ public class ModuloController : Controller
     public IActionResult Criar([FromServices] IRepositoryCurso repositoryCurso)
     {
         ViewBag.Cursos = repositoryCurso.ListarTodosOsCursos();
-        return View();
+        return View("Pages/Modulo/Criar.cshtml");
     }
 }
